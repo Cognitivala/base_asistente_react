0876108dfa679fa9af3818eb36d41a6b7e9d214b
Jose Zuñiga <josezuniga@MacBook-Pro-de-Jose.local>
2018-11-30 11:01:52 -0300

    test

--- FRONT/www/assets/js/main.js
+++ FRONT/www/assets/js/main.js
@@ -133,7 +133,7 @@ function filterUser() {
         "#new-intent, #new-dialog, #save-dialog-btn, #modal-dialog-edit .button-add.edit-button, a.add-intent.delete, #users-list, #admin-users-btn ,#get-code-btn, [role='killswitch'], "+
         "#intents, #dialog, #url, #killswitch, [data-name='intents'], [data-name='dialogs'],[data-name='url'],[data-name='killswitch'] "
       ).remove();
-      if(pathName === "/") $('[role="uploadfiles"]')[0].click();
+      if(pathName === "/") $('[role="uploadfiles"]')[0].classList.add('active');
       break;
     case "2":
       if(pathName !== "/indicadores/") window.location.href = getPath + "indicadores/";
@@ -141,7 +141,7 @@ function filterUser() {
         "#new-intent, #new-dialog, #save-dialog-btn, #modal-dialog-edit .button-add.edit-button, a.add-intent.delete, #users-list, #admin-users-btn ,#get-code-btn, [role='killswitch'], "+
         "ul.list-intents>li>ul>li.options, #save-intent, #save-url, #list-dialogs-respond-edit a.delete-dialog, #personalizar-asis-btn, #config-asis-btn"
       ).remove();
-      if(pathName === "/") $('[role="uploadfiles"]')[0].click();
+      if(pathName === "/") $('[role="uploadfiles"]')[0].classList.add('active');
       break;
     default:
       break;
