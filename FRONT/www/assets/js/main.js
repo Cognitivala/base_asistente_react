a753b4b1f75967d4a8ece8edbb9de5f098696743
Jose Zuñiga <josezuniga@MacBook-Pro-de-Jose.local>
2018-12-05 09:49:39 -0300

    test download valoraciones

--- FRONT/www/assets/js/main.js
+++ FRONT/www/assets/js/main.js
@@ -226,8 +226,10 @@ function createCSV(content, nameFile) {
   var encodedUri = encodeURI(csvContent);
   var link_2 = document.getElementsByClassName("download-csv");
   for (var i = 0; i < link_2.length; i++) {
-    link_2[i].setAttribute("href", encodedUri);
-    link_2[i].setAttribute("download", nameFile + ".csv");
+    if(link_2[i].id===""){
+      link_2[i].setAttribute("href", encodedUri);
+      link_2[i].setAttribute("download", nameFile + ".csv");
+    }
   }
 }
 
