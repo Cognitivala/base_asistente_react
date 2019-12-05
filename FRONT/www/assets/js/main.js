eb892e1148e21937c4f3b4375e31259ce8c52efd
mrise <milae.rise@gmail.com>
2019-12-05 17:06:04 -0300

    TEST:

--- FRONT/www/assets/js/main.js
+++ FRONT/www/assets/js/main.js
@@ -143,7 +143,7 @@ function filterUser() {
       //   "ul.list-intents>li>ul>li.options, #save-intent, #save-url, #list-dialogs-respond-edit a.delete-dialog, #personalizar-asis-btn, #config-asis-btn"
       // ).remove();
       // if(pathName === "/") {$('[role="uploadfiles"]')[0].classList.add('active'); $('#upload-file').addClass('in').addClass('active') }
-      $('.editorOnly').remove();
+      $('.editorOnly', '#save-url').remove();
       $('.editorOnlyDisabled').prop('disabled', true);
       break;
     default:
