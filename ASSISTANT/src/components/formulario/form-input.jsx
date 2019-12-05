c99b44b01b3fa499c42767b9968b8d85afc6c45b
Oscar Cornejo <oscarcornejo10@gmail.com>
2019-12-05 15:04:23 -0300

    UPDATE: Se cambia valor PropTypes del form-input.jsx

--- ASSISTANT/src/components/formulario/form-input.jsx
+++ ASSISTANT/src/components/formulario/form-input.jsx
@@ -42,5 +42,5 @@ FormInput.propTypes = {
   validate: PropTypes.object,
   withError: PropTypes.bool,
   mainCss: PropTypes.object.isRequired,
-  value: PropTypes.value
+  value: PropTypes.string
 };
