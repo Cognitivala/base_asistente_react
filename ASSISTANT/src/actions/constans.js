959d83d9b645d0883830455d756436cfd69479ed
Oscar Cornejo <oscarcornejo10@gmail.com>
2019-12-13 16:37:57 -0300

    UPDATE: Se agrega pagina test y se actualizan url de endpoint

--- ASSISTANT/src/actions/constans.js
+++ ASSISTANT/src/actions/constans.js
@@ -1,2 +1,3 @@
-export const APIURL = "https://dev-sura.mycognitiva.io/mad";
+export const APIURL = "https://dev-sura-api.mycognitiva.io/mad";
+// export const APIURL = "https://dev-sura.mycognitiva.io/mad";
 // export const APIURL = "https://asistente.cognitiva.la/mad";
