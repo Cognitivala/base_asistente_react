7bd718e338051ac972ef3c13ff62c83fe6e4651f
Oscar Cornejo <oscarcornejo10@gmail.com>
2019-12-13 17:10:11 -0300

    UPDATE: Se actualizan url

--- CDN/www/config/url.js
+++ CDN/www/config/url.js
@@ -1,3 +1,4 @@
 export const urlAsistente = {
-    endpoint: "https://dev-sura.mycognitiva.io/asistente/",
+    // endpoint: "https://dev-sura.mycognitiva.io/asistente/",
+    endpoint: "https://dev-sura-api.mycognitiva.io/asistente/",
 };
