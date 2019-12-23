e7884b30910bd7481411a4498535150ac6c265f0
Oscar Cornejo <oscarcornejo10@gmail.com>
2019-12-23 15:35:09 -0300

    WIP: Se revisa problema formulario en DEV

--- ASSISTANT/src/components/formValoracion/FormularioValoracion.js
+++ ASSISTANT/src/components/formValoracion/FormularioValoracion.js
@@ -1,78 +1,72 @@
 import React, { Component } from "react";
-import Star from './Star';
+import Star from "./Star";
 
-import './FormValoracion.scss';
+import "./FormValoracion.scss";
 
 const style = {
-    boxStar: {
-        width: '100%',
-        display: 'flex',
-        alignItems:'flex-end',
-        justifyContent: 'flex-start',
-        marginBottom: '10px',
-    }
-}
+  boxStar: {
+    width: "100%",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "flex-start",
+    marginBottom: "10px"
+  }
+};
 
 const totalStars = 5;
 
 class FormularioValoracion extends Component {
   state = {
-    respuesta: "si",
+    respuesta: null,
     starsSelected: 0,
     mensajeAdicional: ""
   };
 
-  handleOptionChange = (e) => {
+  handleOptionChange = e => {
     console.log(e.target.value);
     if (e.target.value) {
-        this.setState({
-            ...this.state,
-            respuesta: e.target.value
-        });
+      this.setState({ ...this.state, respuesta: e.target.value });
     } else {
-        this.setState({
-            ...this.state,
-            respuesta: e.target.value
-        });
+      this.setState({ ...this.state, respuesta: e.target.value });
     }
-};
+  };
 
-    
-    enviarValoracion = (e) => {
-        e.preventDefault();
-        if(this.state.respuesta === null || this.state.starsSelected === 0 || this.state.mensajeAdicional === '') {
-            return false;
-        }
-
-        const {generalStates, sendValoracion} = this.props;
-        const general = generalStates.toJS();
-
-        let resolvio = null;
-        if (this.state.respuesta === 'si') {
-            resolvio = 1;
-        } else {
-            resolvio = 0;
-        }
-
-        const data = {
-            input: "in",
-            output: "out",
-            cid: general.cid,
-            id_data_canal: 123,
-            id_canal: 1,
-            resolvio: resolvio,
-            valoracion: this.state.starsSelected,
-            comentario: this.state.mensajeAdicional,
-        }
-        console.log('DATA VALORACIÓN:: ', data);
-        // sendLike(data, general);
-       sendValoracion(data, general);
+  enviarValoracion = e => {
+    e.preventDefault();
+    if (
+      this.state.respuesta === null ||
+      this.state.starsSelected === 0 ||
+      this.state.mensajeAdicional === ""
+    ) {
+      return false;
     }
+
+    const { generalStates, sendValoracion } = this.props;
+    const general = generalStates.toJS();
+
+    let resolvio = null;
+    if (this.state.respuesta === "si") {
+      resolvio = 1;
+    } else {
+      resolvio = 0;
+    }
+
+    const data = {
+      input: "in",
+      output: "out",
+      cid: general.cid,
+      id_data_canal: 123,
+      id_canal: 1,
+      resolvio: resolvio,
+      valoracion: this.state.starsSelected,
+      comentario: this.state.mensajeAdicional
+    };
+    console.log("DATA VALORACIÓN:: ", data);
+    // sendLike(data, general);
+    sendValoracion(data, general);
+  };
 
   render() {
-
-    
-
     return (
       <div className="conversationBubbleForm Send">
         {/* <img className={mainCss.RoundedImg} src={} alt="" /> */}
@@ -90,106 +84,47 @@ class FormularioValoracion extends Component {
               <legend>¿Su caso o inquietud fueron resueltas?</legend>
               <label>
                 <div className="round">
-                  <div
-                    className={this.state.respuesta === "si" ? "active circle" : " circle"}
-                  ></div>
+                  <div className={ this.state.respuesta === "si" ? "active circle" : " circle" }></div>
                   Sí
-                  <input
-                    type="radio"
-                    name="desicion"
-                    value="si"
-                    checked={this.state.respuesta === "si"}
-                    onChange={this.handleOptionChange}
-                  />
+                  <input type="radio" name="desicion" value="si" checked={this.state.respuesta === "si"} onChange={this.handleOptionChange} />
                 </div>
               </label>
               <label>
                 <div className="round">
-                  <div
-                    className={this.state.respuesta === "no" ? "active circle" : " circle"}
-                  ></div>
+                  <div className={ this.state.respuesta === "no" ? "active circle" : " circle" }></div>
                   No
-                  <input
-                    type="radio"
-                    name="desicion"
-                    value="no"
-                    checked={this.state.respuesta === "no"}
-                    onChange={this.handleOptionChange}
-                  />
+                  <input type="radio" name="desicion" value="no" checked={this.state.respuesta === "no"} onChange={this.handleOptionChange} />
                 </div>
               </label>
             </fieldset>
 
             <fieldset>
-              {/* <StarRating totalStars={5} /> */}
 
               <div className="star-rating">
                 <div style={style.boxStar}>
                   {[...Array(totalStars)].map((n, i) => (
-                    <Star
-                      key={i}
-                      selected={i < this.state.starsSelected}
-                      onClick={() => this.setState({starsSelected: i + 1})}
-                    />
+                    <Star key={i} selected={i < this.state.starsSelected} onClick={() => this.setState({ starsSelected: i + 1 })} />
                   ))}
                 </div>
 
                 <div>
-                  {this.state.starsSelected === 0 && <p>Seleccione su valoración</p>}
-                  {this.state.starsSelected === 1 && (
-                    <p>
-                      Muy insatisfecho{" "}
-                      <span role="img" aria-label="">
-                        😡
-                      </span>
-                    </p>
+                  {this.state.starsSelected === 0 && (
+                    <p>Seleccione su valoración</p>
                   )}
-                  {this.state.starsSelected === 2 && (
-                    <p>
-                      No fue de mucha ayuda{" "}
-                      <span role="img" aria-label="">
-                        😞
-                      </span>
-                    </p>
-                  )}
-                  {this.state.starsSelected === 3 && (
-                    <p>
-                      Me ayudó, pero necesita mejorar{" "}
-                      <span role="img" aria-label="">
-                        😐
-                      </span>
-                    </p>
-                  )}
-                  {this.state.starsSelected === 4 && (
-                    <p>
-                      ¡Buen servicio!{" "}
-                      <span role="img" aria-label="">
-                        🙂
-                      </span>
-                    </p>
-                  )}
-                  {this.state.starsSelected === 5 && (
-                    <p>
-                      ¡Excelente servicio!{" "}
-                      <span role="img" aria-label="">
-                        😃
-                      </span>
-                    </p>
-                  )}
+                  {this.state.starsSelected === 1 && ( <p> Muy insatisfecho <span role="img" aria-label=""> 😡</span></p> )}
+                  {this.state.starsSelected === 2 && ( <p>No fue de mucha ayuda <span role="img" aria-label=""> 😞</span></p> )}
+                  {this.state.starsSelected === 3 && ( <p>Me ayudó, pero necesita mejorar <span role="img" aria-label=""> 😐</span></p> )}
+                  {this.state.starsSelected === 4 && ( <p>¡Buen servicio! <span role="img" aria-label=""> 🙂</span></p> )}
+                  {this.state.starsSelected === 5 && ( <p>¡Excelente servicio! <span role="img" aria-label=""> 😃</span></p> )}
                 </div>
               </div>
             </fieldset>
 
             <fieldset>
               <legend style={{ fontWeight: 100, marginBottom: "0.8rem" }}>
-                ¡Gracias por la valoración! Nos ayuda a seguir mejorando. Puedes
-                dejar un mensaje adicional en el espacio siguiente
+                ¡Gracias por la valoración! Nos ayuda a seguir mejorando. Puedes dejar un mensaje adicional en el espacio siguiente:
               </legend>
-              <textarea
-                name="mensajeAdicional"
-                rows="2"
-                onChange={e => this.setState({...this.state, mensajeAdicional: e.target.value})}
-              ></textarea>
+              <textarea name="mensajeAdicional" rows="2" onChange={ e => this.setState({ ...this.state, mensajeAdicional: e.target.value }) }></textarea>
             </fieldset>
 
             <fieldset>
