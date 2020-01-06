3c0d2000412638e1c01f5cb6a669c462c4ad95e0
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-01-06 19:55:52 -0300

    UPDATE: Se actualiza variable global para url

--- CDN/www/config/url.js
+++ CDN/www/config/url.js
@@ -1,6 +1,6 @@
 //CAMBIAR URL_API_APP SEGÚN AMBIENTE
 export const URL_API_APP_LOCAL = 'http://localhost:3000';
-export var URL_API_APP = 'https://dev-sura.mycognitiva.io';
+export const URL_API_APP = 'https://dev-sura.mycognitiva.io';
 export const urlAsistente = {
     endpoint: `${URL_API_APP}/asistente/`,
 };
