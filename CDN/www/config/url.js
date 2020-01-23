76917813585a04e79f4b83fba81f67d56f219980
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-01-23 14:07:19 -0300

    UPDATE: Se crea de forma dinámica url de ambiente.

--- CDN/www/config/url.js
+++ CDN/www/config/url.js
@@ -1,6 +1,6 @@
 //CAMBIAR URL_API_APP SEGÚN AMBIENTE
 export const URL_API_APP_LOCAL = 'http://localhost:3000';
-export const URL_API_APP = 'https://dev-coasin-sbk.mycognitiva.io';
+export const URL_API_APP = 'URL_A_CAMBIAR';
 export const urlAsistente = {
-    endpoint: `${URL_API_APP}/asistente/`,
+    endpoint: `https://${URL_API_APP}.mycognitiva.io/asistente/`,
 };
