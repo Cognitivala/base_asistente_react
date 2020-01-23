e84c997cc9bf4fe28f266c5075276ea16a0efa06
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-01-23 15:41:34 -0300

    UPDATE: Url dinámica para Jenkins.

--- CDN/www/config/url.js
+++ CDN/www/config/url.js
@@ -1,6 +1,6 @@
 //CAMBIAR URL_API_APP SEGÚN AMBIENTE
 export const URL_API_APP_LOCAL = 'http://localhost:3000';
-export const URL_API_APP = 'https://qa-sura.mycognitiva.io';
+export const URL_API_APP = 'URL_A_CAMBIAR';
 export const urlAsistente = {
     endpoint: `${URL_API_APP}/asistente/`,
 };
