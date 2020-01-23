73515de24b44dcf130265e35e43abcd9d7ec19ec
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-01-23 15:34:38 -0300

    UPDATE: Url dinámica para Jenkins.

--- ASSISTANT/src/actions/constans.js
+++ ASSISTANT/src/actions/constans.js
@@ -1,3 +1,3 @@
 const ASISTENTE_URL = 'URL_A_CAMBIAR';
-export const APIURL = `https://${ASISTENTE_URL}.mycognitiva.io/mad`;
+export const APIURL = `https://${ASISTENTE_URL}/mad`;
 // // export const APIURL = "https://asistente.cognitiva.la/mad";
