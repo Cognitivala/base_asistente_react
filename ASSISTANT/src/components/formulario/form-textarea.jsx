9caf74c8aace141023bef0a0b01122d85cb22043
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-05 15:41:18 -0300

    WIP: Se revisa subida de archivo en LYNN

--- ASSISTANT/src/components/formulario/form-textarea.jsx
+++ ASSISTANT/src/components/formulario/form-textarea.jsx
@@ -14,10 +14,16 @@ export default class FormTextarea extends Component {
 
   content() {
     const { name,placeholder,autocomplete,rows, validateFunc ,validate, withError } = this.props;
+    const { type, mainCss, value } = this.props;
+
+    console.log(type);
+    console.log(mainCss);
+    console.log(value);
+
     let cssClass = withError?" error":"";
 
     console.log('VALIDATE:: ', validate);
-    console.log('validateFunc:: ', validateFunc);
+    // console.log('validateFunc:: ', validateFunc);
 
     return (
         <textarea minLength="1" maxLength='1000' name={name} placeholder={placeholder} autoComplete={autocomplete} 
