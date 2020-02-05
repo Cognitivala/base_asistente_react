c869da305392949c9c8b687fd2af735453cd3d0f
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-05 16:57:53 -0300

    WIP: Se revisa estructura de burbuja

--- ASSISTANT/src/components/launcher/notification-circle.jsx
+++ ASSISTANT/src/components/launcher/notification-circle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotificationCircle = (props) => {
   return (
-    <div>
+    <div style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
       <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
     </div>
   )
