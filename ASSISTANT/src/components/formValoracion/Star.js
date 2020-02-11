f6b44ab7b920646eaa2ad8b33248dd87375718be
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-11 15:24:14 -0300

    UPDATE: Se aplica variable de color para formulario de valoracion (encuesta)

--- ASSISTANT/src/components/formValoracion/Star.js
+++ ASSISTANT/src/components/formValoracion/Star.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const Star = ({ colorHeader, selected = false, onClick = f => f }) => {
-    return ( <
-        div className = { selected ? "star selected" : "star" }
-        onClick = { onClick }
-        style = { colorHeader ? { backgroundColor: `${colorHeader}` } : null }
-        />
+    return (
+        <div className={selected ? "star selected" : "star"} onClick={onClick} style={ selected && colorHeader ? { backgroundColor: `${colorHeader}` } : null } />
     )
 };
 
-export default Star;
+  export default Star;
