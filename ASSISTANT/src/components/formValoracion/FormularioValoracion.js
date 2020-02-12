7de39a9fedbb2bb9f575a4cff3255995d4169658
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-12 15:45:04 -0300

    UPDATE: Se aplica variable de color para formulario de valoracion (encuesta)

--- ASSISTANT/src/components/formValoracion/FormularioValoracion.js
+++ ASSISTANT/src/components/formValoracion/FormularioValoracion.js
@@ -184,8 +184,8 @@ class FormularioValoracion extends Component {
               </legend>
               <textarea onKeyUp={this.limpiarError.bind(this)} style={ this.state.campoRequerido ? { border: '.1rem solid #ff2200' } : null} name="mensajeAdicional" rows="2" onChange={ e => this.setState({ ...this.state, mensajeAdicional: e.target.value }) }></textarea>
               
-              { this.state.campoRequerido && this.state.starsSelected > 3 && <legend style={{color: '#ff2200'}}>*Este campo es obligatorio</legend> }
-              {/* { this.state.campoRequerido && <legend style={{color: '#ff2200'}}>*Este campo es obligatorio</legend> } */}
+              { this.state.campoRequerido && this.state.mensajeAdicional === "" ? <legend style={{color: '#ff2200'}}>*Este campo es obligatorio</legend> : null}
+              { this.state.campoRequerido && this.state.starsSelected > 3 ? <legend style={{color: '#ff2200'}}>*Este campo es obligatorio</legend> : null}
               
             </fieldset>
 
