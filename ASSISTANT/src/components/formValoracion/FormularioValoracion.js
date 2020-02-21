95182a98f17c12877722c6a0025acb689a0629a4
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-21 16:46:41 -0300

    UPDATE: Se agrega validación para minimo de caracteres.

--- ASSISTANT/src/components/formValoracion/FormularioValoracion.js
+++ ASSISTANT/src/components/formValoracion/FormularioValoracion.js
@@ -106,7 +106,7 @@ class FormularioValoracion extends Component {
   limpiarError() {
 
     console.log(this.state.starsSelected);
-    console.log(this.state.mensajeAdicional);
+    console.log(this.state.minCaracteres);
     console.log(this.state.mensajeAdicional.length);
 
     if (this.state.starsSelected <= 3 && this.state.mensajeAdicional.length <= 10){
@@ -224,7 +224,7 @@ class FormularioValoracion extends Component {
                 )
               } */}
               { this.state.campoRequerido && this.state.mensajeAdicional === "" && this.state.starsSelected <= 3 && <legend style={{color: '#ff2200'}}>*Este campo es obligatorio</legend> }
-              { this.state.minCaracteres && <legend style={{color: '#ff2200'}}>*Debe ingresar mínimo 10 carácteres</legend> }
+              { this.state.minCaracteres && this.state.mensajeAdicional.length <= 10 && <legend style={{color: '#ff2200'}}>*Debe ingresar mínimo 10 carácteres</legend> }
               
             </fieldset>
 
