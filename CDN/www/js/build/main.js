34543e3f5aa8ead8f82cc5751c3791e1334a13ac
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-26 17:21:12 -0300

    UPDATE: Se agrega logo de Cognitiva.

--- CDN/www/js/build/main.js
+++ CDN/www/js/build/main.js
@@ -32,14 +32,14 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
         var _this = this;
 
         var div = document.createElement("div"),
-            ifrm = document.createElement("object");
+            ifrm = document.createElement("iframe");
         ifrm.setAttribute("id", "ifrm-assitant");
         ifrm.classList.add("iframe-cognitive-assistant-container");
         ifrm.classList.add("active");
         ifrm.classList.add("notification");
         ifrm.setAttribute("allow", "geolocation *;");
         ifrm.setAttribute("allow", "microphone *;");
-        ifrm.setAttribute("data", this.src);
+        ifrm.setAttribute("src", this.src);
         ifrm.setAttribute("data-origin", this.origin);
         div.appendChild(ifrm);
         document.body.appendChild(div);
@@ -101,7 +101,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
             style = document.createElement("style");
         css += ".iframe-cognitive-assistant-container {overflow: hidden;border: none;position: fixed;bottom: 10px;right: 10px;background-color: transparent;opacity: 0;transform: translate3d(0,100%,0);transition: transform opacity height 350ms ease;pointer-events: none;transition-timing-function: cubic-bezier(0.56, 1.19, 0.2, 1.05);z-index: 99999999;border-radius: 5px;}";
         css += ".iframe-cognitive-assistant-container.active {opacity: 1;transform: translate3d(0,0,0);pointer-events: auto;}";
-        css += ".iframe-cognitive-assistant-container.notification{ height: 320px; width: 100%;max-width: 365px;box-shadow: none;right: 0;bottom: 0; }";
+        css += ".iframe-cognitive-assistant-container.notification{ height: 200px; width: 220px;max-width: 365px;box-shadow: none;right: 0;bottom: 0; }";
         css += ".iframe-cognitive-assistant-container.assistant{ height: 100vh;width: 100%;max-width: 401px;right: 0px;bottom: 0px; }";
         css += ".iframe-cognitive-assistant-container.minimized{ height: 100px;width: 100%;max-width: 100px;right: 10px;bottom: 0px;}";
         css += ".cognitive-iframe {height: 100%;width: 100%;border: 0 none;}";
