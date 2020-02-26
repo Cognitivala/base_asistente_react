af3e6ff786b6ba6fc440812118bd3e205cd4eaf4
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-02-26 15:42:20 -0300

    UPDATE: Se cambia url de asist a aistente

--- CDN/www/config/url.js
+++ CDN/www/config/url.js
@@ -2,5 +2,5 @@
 export const URL_API_APP_LOCAL = 'http://localhost:3000';
 export const URL_API_APP = 'https://dev-sudmedica.mycognitiva.io';
 export const urlAsistente = {
-    endpoint: `${URL_API_APP}/asist/`,
+    endpoint: `${URL_API_APP}/asistente/`,
 };
