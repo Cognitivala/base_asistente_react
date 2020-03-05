11f97527e4f3cf26bedf1d1323016ac140fa7ba4
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-03-05 17:49:38 -0300

    UPDATE: Se ordena código.

--- ASSISTANT/src/components/launcher/notification-circle.jsx
+++ ASSISTANT/src/components/launcher/notification-circle.jsx
@@ -4,8 +4,9 @@ const NotificationCircle = (props) => {
   // console.log('NotificationCircle:: ', props);
   return (
     // <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
+      // <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
     <div>
-      <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
+      <span className={props.mainCss.BubbleLauncher}>1</span>
     </div>
   )
 };
