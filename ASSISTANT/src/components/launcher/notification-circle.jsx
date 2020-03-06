f1d10665450db73c2ac9af7c58a10f01d3b14108
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-03-06 15:35:35 -0300

    UPDATE: Se ordena código.

--- ASSISTANT/src/components/launcher/notification-circle.jsx
+++ ASSISTANT/src/components/launcher/notification-circle.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const NotificationCircle = (props) => {
   // console.log('NotificationCircle:: ', props);
   return (
-    <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
+    <div>
+      <span className={props.mainCss.BubbleLauncher} style={props.bubbleLogo === '' ? {left: '13.5rem'} : null}>1</span>
+    </div>
   )
 };
 
