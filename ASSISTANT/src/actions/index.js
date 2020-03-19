a51bdc81239a5f1f55d391fbc56b1dc2efd3f4f1
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-03-19 17:28:41 -0300

    UPDATE: Se agrega estilos al modal de asistencia.

--- ASSISTANT/src/actions/index.js
+++ ASSISTANT/src/actions/index.js
@@ -541,7 +541,11 @@ export function updateConversation(data) {
                 rut: getUrlParams(getState, 'rut'),
                 user: getUrlParams(getState, 'user'),
                 clave: getUrlParams(getState, 'clave'),
-                general: {...data.general.integracion, email_user: data.email_user }
+                general: {
+                    ...data.general,
+                    email_user: data.email_user
+                }
+                // general: {...data.general.integracion, email_user: data.email_user }
                 // email_user: getUrlParams(getState, 'ejecutivo_amsa')
             }
         });
