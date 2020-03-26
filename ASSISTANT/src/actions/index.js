043399ba1e8f3432df72f58d2dc7d768cf85a3bb
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-03-26 18:21:25 -0300

    WIP: Se agrega métodos para endpoints Sixbell In/Out

--- ASSISTANT/src/actions/index.js
+++ ASSISTANT/src/actions/index.js
@@ -1223,36 +1223,33 @@ export function getSixbellIn(data) {
 
 //SIXBELL OUT
 export function getSixbellOut(data) {
-
     // clearInterval(asistantInterval);
 
-    return function action(dispatch) {
+    var ASISTANT_INTERVAL_TIMER = 5000;
 
-        var ASISTANT_INTERVAL_TIMER = 5000;
-        var asistantInterval = setInterval(function() {
-            const urlApi = 'https://minsal.mycognitiva.io/mad/sixbell_purecloud_out'
-            const data = { data: '' }
-            const token = sessionStorage.getItem("token");
+    var asistantInterval = setInterval(function() {
+        const urlApi = 'https://minsal.mycognitiva.io/mad/sixbell_purecloud_out'
+        const { general, msg, send, enabled } = data;
+        let newData = { general, msg, send, enabled }
+        const token = sessionStorage.getItem("token");
 
-            axios.post(urlApi, data, {
-                headers: {
-                    // 'Authorization': `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                }
-            }).then((response) => {
-                console.log('getSixbellOut:: ', response.data);
-                const dataResponse = response.data;
+        axios.post(urlApi, newData, {
+            headers: {
+                // 'Authorization': `Bearer ${token}`,
+                "Content-Type": "application/json",
+            }
+        }).then((response) => {
+            console.log('getSixbellOut:: ', response.data);
+            const dataResponse = response.data;
 
-                if (dataResponse.status === 200 || dataResponse.datos.length !== 0) {
-                    // dispatch(updateConversation(response.data.msg));
-                } else {
+            if (dataResponse.status === 200 || dataResponse.datos.length !== 0) {
+                // dispatch(updateConversation(response.data.msg));
+            } else {
 
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
 
-        }, ASISTANT_INTERVAL_TIMER);
-
-    }
+    }, ASISTANT_INTERVAL_TIMER);
 }
