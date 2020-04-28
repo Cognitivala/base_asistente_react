3833cfa0eb0d3192542070a053c0728e6a5bb39b
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-04-27 20:11:53 -0400

    UPDATE: Se detecta viewport de dispositivos mobiles

--- ASSISTANT/src/components/launcher/launcher.jsx
+++ ASSISTANT/src/components/launcher/launcher.jsx
@@ -53,7 +53,7 @@ export default class Launcher extends Component {
 
     console.log('pathname: ', pathname);
 
-    if( (pathname === '/asistente/' || src === '?open=true') && window.outerWidth <= 767){
+    if((pathname === '/asistente/' && window.outerWidth <= 767) || (src === '?open=true' && window.outerWidth <= 767)){
       const { closeLauncher, closeHelp, openAssistant, ayudaStates } = this.props;
       closeLauncher();
       this.openAssitantCDN();
