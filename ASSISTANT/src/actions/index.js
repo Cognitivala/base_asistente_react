7f33058f2738989cc5359bbc3f37c33a0a5b55e4
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-07-06 17:33:30 -0400

    WIP: prueba para cargar polizas

--- ASSISTANT/src/actions/index.js
+++ ASSISTANT/src/actions/index.js
@@ -533,6 +533,7 @@ function updateConversationError(data) {
     return { type: "PUSH_CONVERSATIONS_ERROR", data: conv };
 }
 export function updateConversation(conversationData) {
+    console.log('conversationData:: ', conversationData);
     return function action(dispatch, getState) {
         const useLynn = getState().assistantStates.getIn(["useLynn"]);
         const url = useLynn ? LYNN_ENDPOINT : "/message"
