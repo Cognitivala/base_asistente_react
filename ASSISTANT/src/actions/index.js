323ad1e10551facf25809f6fa8e6a9f8598d8800
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-07-29 13:58:12 -0400

    UPDATE: Se crea variable para detecta parámetros desde url de integración

--- ASSISTANT/src/actions/index.js
+++ ASSISTANT/src/actions/index.js
@@ -1203,8 +1203,10 @@ export function updateConversationButton(data) {
                 console.log(localStorage.getItem("previous_input"));
                 console.log(sessionStorage.getItem("previous_input"));
 
-                item.msg = response.data.msg;
-                messageResponse(dispatch, item);
+                if (polizasSitioPrivado) {
+                  item.msg = response.data.msg;
+                  messageResponse(dispatch, item);
+                }
               } else {
                 dispatch(setNodoId(item.msg[item.msg.length - 1]));
                 messageResponse(dispatch, item);
