0e78b4d4f7070ee060bb7ffab6b744515c4ea7e4
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-08-18 12:08:50 -0400

    WIP: Se trabaja en integración de userLike

--- ASSISTANT/src/reducers/assistant.js
+++ ASSISTANT/src/reducers/assistant.js
@@ -5,30 +5,34 @@ export function assistantStates(
     isFetching: false,
     error: "",
     active: false,
-    minimized: false
+    minimized: false,
   }),
   action
 ) {
   switch (action.type) {
     case "OPEN_ASSISTANT":
-        return state.set('active',true);
+      return state.set("active", true);
     case "CLOSE_ASSISTANT":
-        return state.set('active',false);
+      return state.set("active", false);
     case "GET_ASSISTANT_START":
       return state.set("isFetching", true);
     case "TOGGLE_MINIMIZED":
       return state.set("minimized", action.data);
     case "GET_ASSISTANT_END":
-      return state.withMutations(map => {
+      return state.withMutations((map) => {
         map
           .set("isFetching", false)
           .set("error", false)
           .set("customParams", Immutable.fromJS(action.data));
       });
     case "GET_ASSISTANT_ERROR":
-      return state.withMutations(map => {
+      return state.withMutations((map) => {
         map.set("isFetching", false).set("error", action.error);
       });
+    case "USE_LYNN":
+      return state.set("useLynn", action.data);
+    case "LYNN_DATA":
+      return state.set("lynnData", action.data);
     default:
       return state;
   }
